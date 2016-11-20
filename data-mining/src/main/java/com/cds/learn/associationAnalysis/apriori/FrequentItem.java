ea51ca8347cdc836cfa757b3c070bc8046fac8a3
anancds <anancds@163.com>
2016-11-20 16:11:00 +0800

    add apriori

--- data-mining/src/main/java/com/cds/learn/associationAnalysis/apriori/FrequentItem.java
+++ data-mining/src/main/java/com/cds/learn/associationAnalysis/apriori/FrequentItem.java
@@ -3,5 +3,50 @@ package com.cds.learn.associationAnalysis.apriori;
 /**
  * Created by cds on 11/20/16 16:04.
  */
-public class FrequentItem {
+public class FrequentItem implements Comparable<FrequentItem>{
+    // 频繁项集的集合ID
+    private String[] idArray;
+    // 频繁项集的支持度计数
+    private int count;
+    //频繁项集的长度，1项集或是2项集，亦或是3项集
+    private int length;
+
+    public FrequentItem(String[] idArray, int count){
+        this.idArray = idArray;
+        this.count = count;
+        length = idArray.length;
+    }
+
+    public String[] getIdArray() {
+        return idArray;
+    }
+
+    public void setIdArray(String[] idArray) {
+        this.idArray = idArray;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public int compareTo(FrequentItem o) {
+        // TODO Auto-generated method stub
+        Integer int1 = Integer.parseInt(this.getIdArray()[0]);
+        Integer int2 = Integer.parseInt(o.getIdArray()[0]);
+
+        return int1.compareTo(int2);
+    }
 }
