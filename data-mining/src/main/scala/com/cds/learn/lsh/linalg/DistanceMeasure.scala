351cf4819704f4f2216295691fed9bea0bf3d5c8
dongshengchen <anancds@163.com>
2017-02-24 10:04:38 +0800

    Updated

--- data-mining/src/main/scala/com/cds/learn/lsh/linalg/DistanceMeasure.scala
+++ data-mining/src/main/scala/com/cds/learn/lsh/linalg/DistanceMeasure.scala
@@ -1,22 +1,3 @@
-/**
-  * Copyright (c) 2015, zhejiang Unview Technologies Co., Ltd.
-  * All rights reserved.
-  * <http://www.uniview.com/>
-  * -----------------------------------------------------------
-  * Product      :BigData
-  * Module Name  :
-  * Project Name :salut-parent
-  * Package Name :com.uniview.salut.spark.traffic.lsh.linalg
-  * Date Created :2016/6/13
-  * Creator      :c02132
-  * Description  :
-  * -----------------------------------------------------------
-  * Modification History
-  * Date        Name          Description
-  * ------------------------------------------------------------
-  * 2016/6/13      c02132         BigData project,new code file.
-  * ------------------------------------------------------------
-  */
 package com.cds.learn.lsh.linalg
 
 import org.apache.spark.mllib.linalg.{LinalgShim, SparseVector, Vectors}
